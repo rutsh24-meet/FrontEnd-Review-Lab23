92d16e4cdcd1c6af3dc6581a9752b90e745e3914
rutsh24-meet <rut.shpigler@gmail.com>
2023-07-17 18:07:43 +0300

    done

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 
-function changeColor() {
-  document.body.style.backgroundColor = "black";
-}
+  function changeColor() {
+    var body = document.body;
+    var currentColor = body.style.backgroundColor;
+
+    if (currentColor === "black") 
+    {
+      body.style.backgroundColor = "#1c87c9"; // First color
+    }
+    else 
+    {
+      body.style.backgroundColor = "black"; // Second color
+    }
+  } 
